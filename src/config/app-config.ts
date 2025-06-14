205a19321fe8640ef0414487d60c2d077f025acd
Vinicius Marques <vinicius.marques.silva@g.globo>
2025-06-14 20:00:24 -0300

    ci: teste 5

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -8,7 +8,7 @@ export const appConfig = {
     autoUpdate: false,
     generateSW: false,
   },
-  version: '0.0.4',
+  version: '0.0.5',
 };
 
 export const LOG_NAVIGATOR_ROUTER_NAVIGATION_EVENTS = true;
